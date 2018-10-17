9910610100648d40e563f5274406a102b3943d36
cad97 <cad97@cad97.com>
2018-10-16 23:10:15 -0400

    pest_deconstruct -> from-pest
    examples are removed because I'm redoing the derive as pest-ast next

--- src/lib.rs
+++ src/lib.rs
@@ -1,171 +1,143 @@
-//! # pest_deconstruct
-//!
-//! [`PestDeconstructor`] and [`FromPest`] are utilities to ease transforming the "dynamically"
-//! typed Pest parse tree into a statically typed AST.
-#![deny(missing_docs)]
+#[doc(hidden)]
+pub extern crate pest;
+#[doc(hidden)]
+pub extern crate void;
 
-extern crate pest;
-#[allow(unused_imports)] // used via reexport
-#[macro_use]
-extern crate derive;
-
-use pest::{
-    iterators::{Pair, Pairs},
-    RuleType,
+use {
+    pest::{
+        iterators::{Pair, Pairs},
+        RuleType,
+    },
+    std::{error::Error, fmt, marker::PhantomData},
+    void::Void,
 };
-use std::iter::Peekable;
 
-pub use derive::*;
-
-/// Convert from the Pest parse tree to a typed AST node.
-pub trait FromPest<'a>: Sized {
-    /// The rule enum that this AST pairs with.
+/// Potentially borrowing conversion from a pest parse tree.
+pub trait FromPest<'pest>: Sized {
+    /// The rule type for the parse tree this type corresponds to.
     type Rule: RuleType;
-    /// The specific rule that this AST represents.
-    const RULE: Self::Rule;
-    /// Convert from a Pest parse tree node to a new AST node.
+    /// An error type during conversion.
+    type Error;
+    /// Convert from a Pest parse tree.
     ///
-    /// # Panics
-    ///
-    /// If `pest.as_rule() != <Self as FromPest>::RULE`.
-    fn from_pest(pest: Pair<'a, Self::Rule>) -> Self;
+    /// If returning `Ok(_)`, `pest` has been updated to consume the converted `Pair`(s).
+    /// If returning `Err(_)`, `pest` has not been changed.
+    fn from_pest(pest: &mut Pairs<'pest, Self::Rule>) -> Result<Self, Self::Error>;
 }
 
-// FUTURE(rust-lang/rust#46969):
-//    - impl<'a, T: FromPest<'a>> FromPest<'a> for PhantomData<T> {
-//          type Rule: T::Rule;
-//          const RULE = T::RULE;
-//          fn from_pest(pest: Pair<'a, T::Rule>) -> Self {
-//              let _ = T::from_pest(pest);
-//              PhantomData
-//          }
-//      }
-//    - impl<'a, T: FromPest<'a>> FromPest<'a> for Box<T> {
-//          type Rule: T::Rule;
-//          const RULE = T::RULE;
-//          fn from_pest(pest: Pair<'a, T::Rule>) -> Self {
-//              Box::new(T::from_pest(pest))
-//          }
-//      }
-
-/// Deconstruct a Pest `Pair` into its inner productions in a strongly-typed, panic-enforced manner.
-/// See [`PestDeconstructor`] for more information.
-pub trait PestDeconstruct<'i> {
-    /// The rule enum that this pairs with.
-    type Rule: RuleType;
-    /// Create a new [`PestDeconstructor`] to disassemble the Pest parse tree.
-    fn deconstruct(self) -> PestDeconstructor<'i, Self::Rule>;
-}
-
-impl<'i, R: RuleType> PestDeconstruct<'i> for Pair<'i, R> {
-    type Rule = R;
-    fn deconstruct(self) -> PestDeconstructor<'i, R> {
-        PestDeconstructor {
-            pairs: self.into_inner().peekable(),
-        }
+/// Convert a production without storing it.
+impl<'pest, Rule: RuleType, T: FromPest<'pest, Rule = Rule>> FromPest<'pest> for PhantomData<T> {
+    type Rule = Rule;
+    type Error = T::Error;
+    fn from_pest(pest: &mut Pairs<'pest, Rule>) -> Result<Self, Self::Error> {
+        let _ = T::from_pest(pest)?;
+        Ok(std::marker::PhantomData)
     }
 }
 
-/// Deconstruct a Pest `Pair` into its inner productions in a strongly-typed, panic-enforced manner.
+/// For recursive grammars.
+impl<'pest, Rule: RuleType, T: FromPest<'pest, Rule = Rule>> FromPest<'pest> for Box<T> {
+    type Rule = Rule;
+    type Error = T::Error;
+    fn from_pest(pest: &mut Pairs<'pest, Rule>) -> Result<Self, Self::Error> {
+        Ok(T::from_pest(pest)?.into())
+    }
+}
+
+/// Convert an optional production.
+impl<'pest, Rule: RuleType, T: FromPest<'pest, Rule = Rule>> FromPest<'pest> for Option<T> {
+    type Rule = Rule;
+    type Error = Void;
+    fn from_pest(pest: &mut Pairs<'pest, Rule>) -> Result<Self, Void> {
+        let mut clone = pest.clone();
+        Ok(T::from_pest(&mut clone).ok().map(|this| {
+            *pest = clone;
+            this
+        }))
+    }
+}
+
+/// Convert many productions. (If `<T as FromPest>::Error ~= Void`, this will be non-terminating.)
+impl<'pest, Rule: RuleType, T: FromPest<'pest, Rule = Rule>> FromPest<'pest> for Vec<T> {
+    type Rule = Rule;
+    type Error = Void;
+    fn from_pest(pest: &mut Pairs<'pest, Rule>) -> Result<Self, Void> {
+        let mut acc = vec![];
+        while let Ok(this) = T::from_pest(pest) {
+            acc.push(this);
+        }
+        Ok(acc)
+    }
+}
+
+/// Consume a production without doing any processing.
+impl<'pest, Rule: RuleType> FromPest<'pest> for Pair<'pest, Rule> {
+    type Rule = Rule;
+    type Error = NoRemainingPairs;
+    fn from_pest(pest: &mut Pairs<'pest, Rule>) -> Result<Self, NoRemainingPairs> {
+        pest.next().ok_or_else(Default::default)
+    }
+}
+
+#[macro_export]
+#[allow(non_snake_case)]
+macro_rules! impl_FromPest_for_concrete {
+    ($Rule:ty) => {
+        impl<'pest> $crate::FromPest<'pest> for $crate::pest::Span<'pest> {
+            type Rule = $Rule;
+            type Error = $crate::NoRemainingPairs;
+            fn from_pest(pest: &mut Pairs<'pest, Rule>) -> Result<Self, NoRemainingPairs> {
+                Pair::from_pest(pest).map($crate::pest::iterators::Pair::into_span)
+            }
+        }
+
+        impl<'pest> $crate::FromPest<'pest> for () {
+            type Rule = $Rule;
+            type Error = $crate::void::Void;
+            fn from_pest(_: &mut Pairs<'pest, Rule>) -> Result<Self, $crate::void::Void> {
+                Ok(())
+            }
+        }
+    };
+}
+
+/// Error during conversion: no pairs remained to convert.
+#[derive(Debug, Default)]
+pub struct NoRemainingPairs {
+    non_exhaustive: (),
+}
+
+impl Error for NoRemainingPairs {}
+impl fmt::Display for NoRemainingPairs {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "expected any rule; ran out of input")
+    }
+}
+
+mod hidden {
+    pub trait Sealed {}
+}
+
+/// `From` : `Into` :: `FromPest` : `PestInto`. Gives you a `.convert()` on `Pairs`.
 ///
-/// Panics on `Drop` if not exhausted.
-/// Use [`PestDeconstructor::discard`] if you mean to ignore productions.
-#[derive(Clone, Debug)]
-pub struct PestDeconstructor<'i, R: RuleType> {
-    pairs: Peekable<Pairs<'i, R>>,
+/// TODO: This name is horrible _do not keep it_!
+pub trait PestInto<'pest, Rule: RuleType>: hidden::Sealed {
+    /// Convert the head of this pest parse tree into a `FromPest` type.
+    fn convert<T: FromPest<'pest, Rule = Rule>>(&mut self) -> Result<T, T::Error>;
 }
 
-impl<'i, R: RuleType> Drop for PestDeconstructor<'i, R> {
-    fn drop(&mut self) {
-        assert_eq!(
-            self.next_untyped(),
-            None,
-            "PestDeconstructor was not fully exhausted"
-        )
+impl<'pest, Rule: RuleType> hidden::Sealed for Pairs<'pest, Rule> {}
+impl<'pest, Rule: RuleType> PestInto<'pest, Rule> for Pairs<'pest, Rule> {
+    fn convert<T: FromPest<'pest, Rule = Rule>>(&mut self) -> Result<T, T::Error> {
+        T::from_pest(self)
     }
 }
 
-impl<'i, R: RuleType> PestDeconstructor<'i, R> {
-    /// Get the next production from this parse tree node.
-    ///
-    /// Useful for `Node` productions.
-    ///
-    /// # Panics
-    ///
-    /// If there are no remaining productions or the next one is for the wrong rule.
-    pub fn next<T: FromPest<'i, Rule = R>>(&mut self) -> T {
-        T::from_pest(self.next_pair(T::RULE))
-    }
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    /// Get the next production from this parse tree node, if it is the correct rule.
-    ///
-    /// Useful for `Node?` productions.
-    pub fn next_opt<T: FromPest<'i, Rule = R>>(&mut self) -> Option<T> {
-        self.next_pair_opt(T::RULE).map(T::from_pest)
-    }
-
-    /// Get the next productions from this parse tree of a single rule type.
-    /// If the next production is for a different rule, returns an empty vector.
-    ///
-    /// Useful for `Node*` productions.
-    pub fn next_many<T: FromPest<'i, Rule = R>>(&mut self) -> Vec<T> {
-        let mut children = vec![];
-        while let Some(child) = self.next_opt() {
-            children.push(child);
-        }
-        children
-    }
-
-    /// Get the next raw `Pair` of a certain rule type.
-    ///
-    /// # Panics
-    ///
-    /// If there are no remaining productions or the next one is for the wrong rule.
-    pub fn next_pair(&mut self, rule: R) -> Pair<'i, R> {
-        self.next_pair_opt(rule).unwrap_or_else(|| {
-            panic!(
-                "Expected {:?} child, got {:?}",
-                rule,
-                self.pairs.peek().map(Pair::as_rule)
-            )
-        })
-    }
-
-    /// Get the next raw `Pair` if it matches a certain rule.
-    pub fn next_pair_opt(&mut self, rule: R) -> Option<Pair<'i, R>> {
-        if self
-            .pairs
-            .peek()
-            .filter(|pair| pair.as_rule() == rule)
-            .is_some()
-        {
-            self.next_untyped()
-        } else {
-            None
-        }
-    }
-
-    /// Get the next raw `Pair`s while they match a certain rule.
-    pub fn next_pair_many(&mut self, rule: R) -> Vec<Pair<'i, R>> {
-        let mut children = vec![];
-        while let Some(child) = self.next_pair_opt(rule) {
-            children.push(child);
-        }
-        children
-    }
-
-    /// Get the next production, if it exists.
-    ///
-    /// This works without providing a concrete rule, unlike [`PestDeconstructor::next_pair`].
-    pub fn next_untyped(&mut self) -> Option<Pair<'i, R>> {
-        self.pairs.next()
-    }
-
-    /// Discard the remaining productions in this parse tree node.
-    pub fn discard(mut self) {
-        // Cannot move out of Drop type, but we want to forget to run Drop::drop
-        let mut pairs = unsafe { std::mem::uninitialized() };
-        std::mem::swap(&mut self.pairs, &mut pairs);
-        std::mem::forget(self);
-    }
+    #[derive(Copy, Clone, Debug, Ord, PartialOrd, Eq, PartialEq, Hash)]
+    pub enum Rule {}
+    impl_FromPest_for_concrete!(Rule);
 }
